21dd62a8e441410aa17a20759d8e4d6baa46d85f
NKimenai <nilskimenai@hotmail.nl>
2021-11-19 20:11:49 +0100

    client and complaint entity fields, base constructer, getters and setters for spring

--- src/main/java/nl/saxion/cds/housingassociation/models/Complaint.java
+++ src/main/java/nl/saxion/cds/housingassociation/models/Complaint.java
@@ -1,4 +1,59 @@
 package nl.saxion.cds.housingassociation.models;
 
 public class Complaint {
+    private final Long complaintID;
+    private Long homeID;
+    private String category;
+    private int estimatedTime;
+    private int actualTime;
+    private int otherCosts;
+
+
+    public Complaint(Long complaintID) {
+        this.complaintID = complaintID;
+    }
+
+    public Long getComplaintID() {
+        return complaintID;
+    }
+
+    public Long getHomeId() {
+        return homeID;
+    }
+
+    public void setHomeId(Long homeID) {
+        this.homeID = homeID;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public void setEstimatedTime(int estimatedTime) {
+        this.estimatedTime = estimatedTime;
+    }
+
+    public int getActualTime() {
+        return actualTime;
+    }
+
+    public void setActualTime(int actualTime) {
+        this.actualTime = actualTime;
+    }
+
+    public int getOtherCosts() {
+        return otherCosts;
+    }
+
+    public void setOtherCosts(int otherCosts) {
+        this.otherCosts = otherCosts;
+    }
 }
