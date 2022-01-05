7ac4e8febe054de9de91a5e2c677a76cc5d1fbb6
zmaji <502241@student.nl>
2022-01-05 10:37:46 +0100

    Adjusted comments and removed unused code

--- src/main/java/nl/saxion/cds/housingassociation/services/ComplaintService.java
+++ src/main/java/nl/saxion/cds/housingassociation/services/ComplaintService.java
@@ -19,7 +19,7 @@ public class ComplaintService {
         HashMap<String, Integer> topComplaints = new HashMap<>();
         List<Map.Entry<String, Integer>> convertedTopComplaints = new ArrayList<>(topComplaints.entrySet());
 
-        // Add all category variables of a Complaint object into a new List
+        // Add all category variables of a Complaint object into a new List due to not being able to count the frequency in HashMap
         for (Complaint complaint : complaints.values()) {
             categories.add(complaint.getCategory());
         }
@@ -30,7 +30,7 @@ public class ComplaintService {
         int gasCount = Collections.frequency(categories, "gas");
         int otherCount = Collections.frequency(categories, "other");
 
-        // Add name with frequency count into a new List
+        // Add name with frequency count into a new List so it can be ordered with new values
         topComplaints.put("Electricity", electCount);
         topComplaints.put("Water", waterCount);
         topComplaints.put("Gas", gasCount);
@@ -40,27 +40,4 @@ public class ComplaintService {
         convertedTopComplaints.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
         System.out.println(convertedTopComplaints);
     }
-
-
-
-//    public int getTopComplaintType() {
-//        Collection<Complaint> types = complaints.values();
-//
-//        int electCount = Collections.frequency(types, "electricity");
-//        int waterCount = Collections.frequency(types, "water");
-//        int gasCount = Collections.frequency(types, "gas");
-//        int otherCount = Collections.frequency(types, "other");
-//
-//        List<Integer> numbers = new ArrayList<>();
-//
-//        int highestType = 0;
-//        for (Integer type : types) {
-//            if (type > highestType) {
-//                highestType = type;
-//            }
-//        }
-//
-//        return highestType;
-//    }
-
 }
